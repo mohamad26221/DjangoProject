69fe470c220f094d8be927448f8c47a084f42e22
mohamadissmail <garethbale26221@gmail.com>
2024-07-05 01:15:45 -0700

    5

--- subject/settings.py
+++ subject/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/mhmd26221/latest-version/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/mhmd26221/latest-version/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
