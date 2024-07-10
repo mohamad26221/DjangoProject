4e321e51973d626f0da9f2f3ac2162ce0d3a3e83
mohamadissmail <garethbale26221@gmail.com>
2024-07-09 20:22:21 -0700

    setting

--- subject/settings.py
+++ subject/settings.py
@@ -123,13 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    r"C:\Users\Administrator\Desktop\Django-project\venv\subject\staticfiles",
 ]
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/home/mhmd26221/latest-version/media/'
