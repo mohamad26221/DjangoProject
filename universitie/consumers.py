9d73c456afe2de99a6ebb67ecfe58899700a5426
mohamadissmail <garethbale26221@gmail.com>
2024-08-06 16:13:23 -0700

    web socket2

--- universitie/consumers.py
+++ universitie/consumers.py
@@ -2,15 +2,34 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 class MyConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.group_name = 'some_group_name'
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
-        data = json.loads(text_data)
-        # معالجة البيانات
-        self.send(text_data=json.dumps({
-            'message': 'Hello from Django'
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
         }))
